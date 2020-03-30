3089903cc0cecc34bba2b2267139ffe92988cacc
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 23:07:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
-      holiday if item.include?("BBQ")
     end 
   end
     holiday.flatten.compact
