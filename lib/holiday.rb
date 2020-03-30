8fde4e80a30974347fc71be0386d62ce720d777e
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 22:41:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -12,7 +12,7 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day].each do |items|
-    items.to_s.split(" ") << supply
+    items << supply
   end 
 end
 
