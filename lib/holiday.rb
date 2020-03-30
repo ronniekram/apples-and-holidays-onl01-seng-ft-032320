fa173fc8e177ebd2283d378409ece241cbbaaa22
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 23:39:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,12 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-
+  holiday_hash.map do |season|
+    puts "#{season.capitalize()}: "
+    season.map do |holiday, items|
+      puts "#{holiday.to_s.split(" ").capitalize().join(" ")} : #{items.to_s}"
+    end 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
